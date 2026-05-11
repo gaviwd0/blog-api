ca3961d07ea87dafb6b6c63bf6e93f3e6182d11b
Gaviwd0 <gaviwd@gmail.com>
2026-05-11 16:30:48 -0300

    fix(main.ts): error de prefix apiURL

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
   app.setGlobalPrefix('blogapi/v1');
+  await app.listen(process.env.PORT ?? 3000);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
